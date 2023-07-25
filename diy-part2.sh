2e0875da9af6a65ce15c3e050b3992b909142b17
love-dy <113014261+love-dy@users.noreply.github.com>
2023-07-25 10:50:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,13 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.3.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.3.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#删除冲突插件
+rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\|openclash\).*")
+#修改默认主题
+sed -i "s/luci-theme-bootstrap/luci-theme-design/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+#修改默认IP地址
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.3.1/g" ./package/base-files/files/bin/config_generate
+#修改默认主机名
+sed -i "s/hostname='.*'/hostname='OpenWRT'/g" ./package/base-files/files/bin/config_generate
