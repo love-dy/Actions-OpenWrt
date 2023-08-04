5ac7fa9ff00a8818f57ce724258eb2a6c8b2e977
love-dy <113014261+love-dy@users.noreply.github.com>
2023-08-05 02:14:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,11 +15,6 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-#echo 'src-git kiddin https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 
 #Linkease x86_64/arm64
 #echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
